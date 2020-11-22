a03aa4b48406dc463c3d0ebc23b74d3ffc8aae4f
jette <jerndal@gmail.com>
2020-11-22 17:33:18 +0100

    Paths fixed. Moved files from web-app folder to root

--- web-app/js/util.js
+++
@@ -1,7 +0,0 @@
-
-const DOMAIN = 'https://localhost/se/';
-
-const API_KEY = 'Flight%20booker%201.2%20plus%20edition';
-var URL = DOMAIN + 'api/API.php?API_KEY=' + API_KEY;
-
-var URL_STATUS = DOMAIN + 'cgi-bin/status_check?';
